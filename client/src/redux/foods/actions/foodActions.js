0be11f9d51b2a5cc1b110ed1ad7c98dc1a8e0c18
unknown <you@example.com>
2024-03-19 15:58:03 -0300

    platos recibe bien del back

--- client/src/redux/foods/actions/foodActions.js
+++ client/src/redux/foods/actions/foodActions.js
@@ -2,27 +2,28 @@ import axios from "axios";
 import { FOODS, FILTER_FOODS, ORDER_FOODS } from "./types";
 
 export const allFoods = () => async (dispatch) => {
-  try {
-    const response = await axios.get('http://localhost:3000/api/menu');
+ try {
+    const response = await axios.get('http://localhost:4000/api/dishes');
+    console.log('Alimentos obtenidos:', response.data); // Agregar este console.log
     dispatch({
       type: FOODS,
-      payload: response.data.menu
+      payload: response.data // Cambiar de response.data.menu a response.data
     });
-  } catch (error) {
+ } catch (error) {
     console.error('Error al obtener los alimentos:', error);
-  }
+ }
 };
 
 export const filterFoods = (filter) => {
-  return {
+ return {
     type: FILTER_FOODS,
     payload: filter
-  };
+ };
 };
 
 export const orderFoods = (order) => {
-  return {
+ return {
     type: ORDER_FOODS,
     payload: order
-  };
+ };
 };
