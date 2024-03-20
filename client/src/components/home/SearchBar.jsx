1ad3eddcea3d5fbe49649874137508cf6a2a3eae
Arreghini <marreghi@gmail.com>
2024-03-20 20:50:24 -0300

    Actualización SearchBar 20/3

--- client/src/components/home/SearchBar.jsx
+++ client/src/components/home/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios'; 
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -7,17 +7,51 @@ import Select from 'react-select';
 const SearchBar = () => {
   const [checkInDate, setCheckInDate] = useState(null);
   const [checkOutDate, setCheckOutDate] = useState(null);
-  const [numberOfPeople, setNumberOfPeople] = useState('1'); 
+  const [numberOfAdults, setNumberOfAdults] = useState(1); 
+  const [numberOfChildren, setNumberOfChildren] = useState(0); 
   const [availableRooms, setAvailableRooms] = useState([]);
   const [reservationStatus, setReservationStatus] = useState('');
+  const [categoryOptions, setCategoryOptions] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null); 
 
+  useEffect(() => {
+    let updatedCategoryOptions = [];
+  
+    if (numberOfAdults <= 2 && numberOfChildren === 0) {
+      updatedCategoryOptions = [
+        { value: 'standartking', label: 'Standart King' },
+        { value: 'superiorking', label: 'Superior King' },
+        { value: 'juniorking', label: 'Junior King' },
+        { value: 'presidentialsuite', label: 'Presidential Suite' },
+        { value: 'all', label: 'All Categories' }
+      ];
+    } else if (numberOfAdults <= 2 && numberOfChildren > 0) {
+      updatedCategoryOptions = [
+        { value: 'superiorking', label: 'Superior King' },
+        { value: 'presidentialsuite', label: 'Presidential Suite' },
+        { value: 'all', label: 'All Categories' }
+      ];
+    }
+
+    setCategoryOptions(updatedCategoryOptions);
+  }, [numberOfAdults, numberOfChildren]);
+  
   const handleSearch = async () => {
     try {
+      let categoryValue = null;
+      if (selectedCategory && selectedCategory.value !== 'all') {
+        categoryValue = selectedCategory.value;
+      }
+
+      const numberOfPeople = numberOfAdults + numberOfChildren;
+
       const searchData = {
         checkInDate,
         checkOutDate,
-        numberOfPeople
+        numberOfPeople,
+        category: categoryValue
       };
+      
       const response = await axios.get('/ruta-disponibilidad', {
         params: searchData
       });
@@ -26,13 +60,14 @@ const SearchBar = () => {
       console.error('Error al enviar la solicitud de disponibilidad:', error);
     }
   };
+
   const handleReservation = async (roomId) => {
     try {
       const reservationData = {
         roomId,
         checkInDate,
         checkOutDate,
-        numberOfPeople
+        numberOfPeople: numberOfAdults + numberOfChildren
       };
       const response = await axios.post('/ruta-reserva', reservationData);
       setReservationStatus(response.data.message);
@@ -40,70 +75,133 @@ const SearchBar = () => {
       setAvailableRooms(prevRooms => prevRooms.filter(room => room.id !== roomId));
     } catch (error) {
       console.error('Error al enviar la solicitud de reserva:', error);
-      // Manejo de errores de red
+    }
+  };
+
+  const handleAdultsChange = (value) => {
+    const adults = Number(value);
+    const maxAdults = 4; 
+    const maxChildren = 4 - adults; 
+  
+    if (adults <= maxAdults) {
+      setNumberOfAdults(adults);
+    } else {
+      setNumberOfAdults(maxAdults);
+    }
+  
+    if (numberOfChildren > maxChildren) {
+      setNumberOfChildren(maxChildren);
+    }
+  };
+  
+  const handleChildrenChange = (value) => {
+    const children = Number(value);
+    const maxChildren = 4 - numberOfAdults; 
+  
+    if (numberOfAdults + children <= 4) {
+      setNumberOfChildren(children);
+    } else {
+      const remainingChildren = 4 - numberOfAdults;
+      setNumberOfChildren(remainingChildren);
     }
   };
 
   return (
-    <div className="max-w-lg mx-auto p-4 bg-white rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold mb-4">Search for Rooms</h2>
-      <div className="mb-4">
-        <label className="block mb-1">Check-in Date:</label>
+    <div className="mt-8 w-4/6 md:w-2/3 lg:w-1/2 mx-auto p-4 bg-v rounded-lg shadow-md flex flex-wrap">
+      <div className="w-full md:w-auto md:flex-1 md:mr-2 mb-4 md:mb-0 mr-1 ml-2">
+        <label className="block mb-1 text-white ">CHECK-IN</label>
         <DatePicker
-          className="w-full px-3 py-2 border rounded-lg"
+          className="w-full px-3 py-2 border rounded-lg bg-v text-white text-center"
           selected={checkInDate}
           onChange={date => setCheckInDate(date)}
+          minDate={new Date()}
+          dateFormat="dd/MM/yyyy" 
         />
       </div>
-      <div className="mb-4">
-        <label className="block mb-1">Check-out Date:</label>
+      <div className="w-full md:w-auto md:flex-1 md:mr-2 mb-4 md:mb-0 mr-1 ml-1">
+        <label className="block mb-1 text-white text-center">CHECK-OUT</label>
         <DatePicker
-          className="w-full px-3 py-2 border rounded-lg"
+          className="w-full px-3 py-2 border rounded-lg bg-v text-white text-center"
           selected={checkOutDate}
           onChange={date => setCheckOutDate(date)}
+          minDate={checkInDate ? new Date(checkInDate.getTime() + 86400000) : new Date()} 
+          dateFormat="dd/MM/yyyy" 
         />
       </div>
-      <div className="mb-4">
-        <label className="block mb-1">Number of People:</label>
+      <div className="w-full md:w-auto md:flex-1 md:mr-2 mb-4 md:mb-0 mr-1 ml-1" >
+        <label className="block mb-1 text-white text-center">ADULTS</label>
         <Select
-          className="w-full px-3 py-2 border rounded-lg"
+          className="w-full px-3 py-2 border rounded-lg bg-v text-black"
           options={[
             { value: '1', label: '1' },
             { value: '2', label: '2' },
             { value: '3', label: '3' },
             { value: '4', label: '4' },            
           ]}
-          value={{ value: numberOfPeople, label: numberOfPeople }}
-          onChange={option => setNumberOfPeople(option.value)}
+          value={{ value: numberOfAdults.toString(), label: numberOfAdults.toString() }}
+          onChange={option => handleAdultsChange(option.value)}
         />
       </div>
-      <button
-        className="block w-full px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-        onClick={handleSearch}
-      >
-        Search
-      </button>
+      <div className="w-full md:w-auto md:flex-1 md:mr-2 mb-4 md:mb-0 mr-1 ml-1" >
+        <label className="block mb-1 text-white text-center">CHILDREN</label>
+        <Select
+          className="w-full px-3 py-2 border rounded-lg bg-v text-black"
+          options={[
+            { value: '0', label: '0' },
+            { value: '1', label: '1' },
+            { value: '2', label: '2' },
+            { value: '3', label: '3' },            
+          ]}
+          value={{ value: numberOfChildren.toString(), label: numberOfChildren.toString() }}
+          onChange={option => handleChildrenChange(option.value)}
+        />
+      </div>
+      <div className="w-full md:w-auto md:mr-2 mb-4 md:mb-0 ml-1 mr-1">
+        <label className="block mb-1 text-white text-center">CATEGORY</label>
+        <Select
+          className="w-full px-3 py-2 border rounded-lg bg-v text-black" 
+          options={categoryOptions}
+          value={selectedCategory}
+          onChange={option => setSelectedCategory(option)}
+          placeholder="Select Category" 
+        />
+      </div>
+      <div className="w-full md:w-auto mt-4 md:mt-0">
+        <button
+          className="w-full px-4 py-2 mt-9 text-white bg-yellow-500 rounded-lg hover:bg-yellow-600 ml-1 mr-5"
+          onClick={handleSearch}
+        >
+          SEARCH
+        </button>
+      </div>
 
       {availableRooms.length > 0 && (
-        <div className="mt-4">
-          <h3 className="text-lg font-semibold mb-2">Available Rooms</h3>
-          <ul>
-            {availableRooms.map(room => (
-              <li key={room.id} className="mb-2">
-                <span className="mr-2">{room.name} - Price: {room.price}</span>
-                <button
-                  className="px-3 py-1 text-sm text-white bg-green-500 rounded-lg hover:bg-green-600"
-                  onClick={() => handleReservation(room.id)}
-                >
-                  BOOK
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {reservationStatus && <p className="mt-4 text-green-600">{reservationStatus}</p>}
+  <div className="w-full mt-4">
+    <h3 className="text-lg font-semibold mb-2 text-white">AVAILABLE ROOMS</h3>
+    <ul>
+      {availableRooms.map(room => (
+        <li key={room.id} className="mb-4">
+          <div className="flex items-center">
+            <img src={room.imageUrl} alt={room.name} className="mr-4 w-24 h-auto" />
+            <div>
+              <p className="text-white font-semibold mb-1">{room.name}</p>
+              {/* En lo que sigue mostramos el precio calculado por el back que al precio base lo reduce dependiendo 
+              de la relación numero de personas-categoría */}
+              <p className="text-white">{room.calculatedPrice}</p> {/* Mostrar el precio recalculado */}
+            </div>
+          </div>
+          <button
+            className="px-3 py-1 mt-2 text-sm text-white bg-yellow-500 rounded-lg hover:bg-yellow-600"
+            onClick={() => handleReservation(room.id)}
+          >
+            BOOK
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+)}
+      {reservationStatus && <p className="mt-4 text-black 0">{reservationStatus}</p>}
     </div>
   );
 };
