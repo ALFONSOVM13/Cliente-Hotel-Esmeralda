e2d5c1a676b65cef846113c7e3913e0b35728434
unknown <you@example.com>
2024-03-19 22:02:14 -0300

    detailCorrecciones

--- client/src/components/detail/DetailModalRoom.jsx
+++ client/src/components/detail/DetailModalRoom.jsx
@@ -19,9 +19,9 @@ const DetailModalRoom = ({ room }) => {
     <>
       <h2 className="text-white text-2xl font-bold">{room.name}</h2>
       <img className="object-cover w-16 h-16 rounded-t-lg md:h-auto md:w-16 md:rounded-none md:rounded-s-lg" src={room.images[0]} alt={room.name} />
-      <div className="carousel w-full h-full flex items-center justify-center"> 
-        <div id="slide1" className="carousel-item relative w-full h-full"> 
-          <img src={room.images[currentImageIndex]} className="w-full h-full object-cover" />
+      <div className="carousel w-auto h-96 flex items-center justify-center"> 
+        <div id="slide1" className=" carousel-item relative w-full h-full"> 
+          <img src={room.images[currentImageIndex]} className=" w-full h-full object-cover" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <button onClick={handlePrevious} className="btn btn-circle text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-6 h-6 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">❮</button>
 <button onClick={handleNext} className="btn btn-circle text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-6 h-6 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">❯</button>
